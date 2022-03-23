bc15187233b99bbfedcfc0c6b8ff7fd3790b5301
Anton <antonr@elementor.com>
2022-03-23 12:04:42 +0200

    added plugin main plugin

---
+++ ar-testimonials.php
@@ -0,0 +1,68 @@
+<?php /** @noinspection PhpIncludeInspection */
+
+/**
+ * Plugin Name: AR Testimonials
+ * Plugin URI: https://www.wordpress.org/ar-testimonials
+ * Description: Description
+ * Version: 1.0
+ * Requires at least: 5.6
+ * Author: Anton Rotshtein
+ * Author URI: https://examle.net
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: ar-testimonials
+ * Domain Path: /languages
+ */
+
+/*
+MV Slider is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+MV Slider is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with MV Slider. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+*/
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+if (!class_exists('AR_Testimonials')) {
+    class AR_Testimonials
+    {
+        function __construct() {
+            $this->define_constants();
+        }
+
+        public function define_constants() {
+            define('MV_SLIDER_PATH', plugin_dir_path(__FILE__));
+            define('MV_SLIDER_URL', plugin_dir_url(__FILE__));
+            define('MV_SLIDER_VERSION', '1.0.0');
+        }
+
+        public static function activate() {
+
+        }
+
+        public static function deactivate() {
+
+        }
+
+        public static function uninstall() {
+
+        }
+
+    }
+}
+
+if (class_exists('AR_Testimonials')) {
+    register_activation_hook(__FILE__, array('AR_Testimonials', 'activate'));
+    register_deactivation_hook(__FILE__, array('AR_Testimonials', 'deactivate'));
+    register_uninstall_hook(__FILE__, array('AR_Testimonials', 'uninstall'));
+
+    $mv_slider = new AR_Testimonials();
+}
